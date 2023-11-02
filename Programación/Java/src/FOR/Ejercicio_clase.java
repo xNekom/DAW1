58ddf69a991a7bc7b71beea504e259d88bae6611
xNekom <31441072+xNekom@users.noreply.github.com>
2023-11-02 13:38:52 +0000

    .

---
+++ Programación/Java/src/FOR/Ejercicio_clase.java
@@ -0,0 +1,11 @@
+package FOR;
+import java.util.*;
+
+public class Ejercicio_clase {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+    
+}
