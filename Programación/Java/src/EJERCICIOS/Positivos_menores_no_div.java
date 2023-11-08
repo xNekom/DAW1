80f5f20c80032a50839ab4139cc659010ed6bcdb
xNekom <pedrojuuu@gmail.com>
2023-11-08 15:36:52 +0100

    .

---
+++ Programación/Java/src/EJERCICIOS/Positivos_menores_no_div.java
@@ -0,0 +1,12 @@
+package EJERCICIOS;
+
+/*Escribe un programa que muestre por pantalla todos los números enteros positivos menores a uno leído por teclado
+que no sean divisibles entre otro también leído de igual forma.
+El que sea divisible quiere decir que la división de un cociente entero sin decimales, o lo que es lo mismo que el resto de cero.
+Se debe controlar la entrada de datos.*/
+
+import java.util.Scanner;
+
+public class Positivos_menores_no_div {
+    public static void main(String[] args) {
+       Scanner entrada = new Scanner(System.in);
