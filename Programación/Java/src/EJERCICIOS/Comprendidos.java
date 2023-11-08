7fef714cdbef563db0562eb893e508bad75bc1af
xNekom <pedrojuuu@gmail.com>
2023-11-08 18:03:19 +0100

    Notas

--- Programación/Java/src/EJERCICIOS/Comprendidos.java
+++ Programación/Java/src/EJERCICIOS/Comprendidos.java
@@ -7,21 +7,33 @@ import java.util.Scanner;
 
 public class Comprendidos {
     public static void main(String[] args) {
-        Scanner entrada = new Scanner(System.in); // Crea un objeto Scanner para leer la entrada del usuario
-        System.out.println("Introduce el primer número entero:"); // Pide al usuario que introduzca el primer número entero
-        int num1 = entrada.nextInt(); // Lee el número entero que el usuario introdujo y lo guarda en la variable num1
-        System.out.println("Introduce el segundo número entero:"); // Pide al usuario que introduzca el segundo número entero
-        int num2 = entrada.nextInt(); // Lee el número entero que el usuario introdujo y lo guarda en la variable num2
+        Scanner entrada = new Scanner(System.in);
 
-        if (num1 == num2) { // Verifica si los números son iguales
-            System.out.println("Los números introducidos deben ser distintos."); // Imprime un mensaje de error si los números son iguales
+        System.out.println("Introduce el primer número entero:");
+        int num1 = entrada.nextInt();
+
+        System.out.println("Introduce el segundo número entero:");
+        int num2 = entrada.nextInt();
+
+        if (num1 == num2) { // Verificamos si los números son iguales
+            System.out.println("Los números introducidos deben ser distintos."); // Imprimimos un mensaje de error si
+                                                                                 // los números son iguales
         } else {
-            int menor = Math.min(num1, num2); // Encuentra el número menor
-            int mayor = Math.max(num1, num2); // Encuentra el número mayor
+            int menor, mayor;
+            if (num1 < num2) { // Si el primer número es menor que el segundo
+                menor = num1; // El primer número es el menor
+                mayor = num2; // El segundo número es el mayor
+            } else { // Si el segundo número es menor que el primero
+                menor = num2; // El segundo número es el menor
+                mayor = num1; // El primer número es el mayor
+            }
 
-            System.out.println("Los números enteros comprendidos entre " + menor + " y " + mayor + " incrementando de 8 en 8 son:"); // Imprime un mensaje que indica los números enteros comprendidos entre los números introducidos
-            for (int i = menor; i <= mayor; i += 8) { // Este bucle controla el número de números enteros que se imprimen
-                System.out.println(i); // Imprime el número entero actual
+            System.out.println("Los números enteros comprendidos entre " + menor + " y " + mayor
+                    + " incrementando de 8 en 8 son:"); // Imprimimos un mensaje que indica qué números se van a mostrar
+
+            for (int i = menor; i <= mayor; i += 8) { // Iniciamos un ciclo for que va desde el número menor hasta el
+                                                      // número mayor, incrementando de 8 en 8
+                System.out.println(i); // Mostramos el número actual
             }
         }
     }
--- Programación/Java/src/EJERCICIOS/Positivos_menores_no_div.java
+++ Programación/Java/src/EJERCICIOS/Positivos_menores_no_div.java
@@ -12,8 +12,27 @@ todos los números enteros positivos menores que el número límite que no sean 
 Para controlar la entrada de datos, puedes utilizar un bucle do-while para asegurarte de que los números introducidos son enteros positivos. 
 También puedes utilizar una declaración if para comprobar si un número es divisible por el otro.*/
 
-import java.util.Scanner;
+import java.util.Scanner; // Importamos la clase Scanner para poder leer la entrada del usuario
 
-public class Positivos_menores_no_div {
+public class Main {
     public static void main(String[] args) {
-       Scanner entrada = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduce un número entero positivo como límite:"); 
+        int limite = scanner.nextInt();
+
+        System.out.println("Introduce otro número entero positivo como divisor:");
+        int divisor = scanner.nextInt();
+
+        System.out.println("Los números enteros positivos menores a " + limite + " que no son divisibles entre "
+                + divisor + " son:");
+
+        for (int i = 1; i < limite; i++) { // Iniciamos un ciclo for que va desde 1 hasta el número límite ingresado por
+                                           // el usuario
+            if (i % divisor != 0) { // Si el número actual no es divisible por el número divisor ingresado por el
+                                    // usuario
+                System.out.println(i); // Mostramos el número actual
+            }
+        }
+    }
+}
