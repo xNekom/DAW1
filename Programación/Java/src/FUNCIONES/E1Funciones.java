42fb27e42af493de88d2e119d0fed08977b83ef7
xNekom <pedrojuuu@gmail.com>
2023-11-13 17:27:21 +0100

    13/11/2023

--- Programación/Java/src/FUNCIONES/E1Funciones.java
+++ Programación/Java/src/FUNCIONES/E1Funciones.java
@@ -13,11 +13,11 @@ public class E1Funciones {
         double num1 = entrada.nextDouble();
         System.out.println("Introduce el segundo número: ");
         double num2 = entrada.nextDouble();
-        double resultado = suma(num1, num2);
-        System.out.println("El resultado de la suma es: " + resultado);
+        double resultado = suma(num1, num2); // Llama a la función suma y almacena el resultado en la variable resultado
+        System.out.println("El resultado de la suma es: " + resultado); // Imprime el resultado de la suma
     }
 
-    public static double suma(double num1, double num2) {
-        return num1 + num2;
+    public static double suma(double num1, double num2) { // Define la función suma que toma dos parámetros num1 y num2
+        return num1 + num2; // Devuelve la suma de num1 y num2
     }
 }
