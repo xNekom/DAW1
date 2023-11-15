cb7f4a3c8f9d7f8d7032517a3c2ec9da1a8a3386
xNekom <pedrojuuu@gmail.com>
2023-11-15 18:57:10 +0100

    15/11/2023

--- Programación/Java/src/FUNCIONES/E3Par_Impar_NOTA.java
+++ Programación/Java/src/FUNCIONES/E3Par_Impar_NOTA.java
@@ -3,6 +3,7 @@ package FUNCIONES;
 Hemos hecho una función que nos muestra por consola si un número es par o impar.
 
 Ayúdate de ambas funciones (desde el método main) para saber si la suma de dos números pedidos por consola es par o impar (quitando la parte decimal).*/
+
 import java.util.Scanner;
 
 public class E3Par_Impar_NOTA {
@@ -10,23 +11,26 @@ public class E3Par_Impar_NOTA {
         Scanner entrada = new Scanner(System.in);
         System.out.println("Introduce el primer número: ");
         double num1 = entrada.nextDouble();
+        parImpar((int) num1);
         System.out.println("Introduce el segundo número: ");
         double num2 = entrada.nextDouble();
-        double resultado = suma(num1, num2); // Llama a la función suma y almacena el resultado en la variable resultado
-        System.out.println("El resultado de la suma es: " + resultado); // Imprime el resultado de la suma
+        parImpar((int) num2);
+        double resultado = suma(num1, num2);
+        System.out.println("El resultado de la suma es: " + resultado);
         parImpar((int)resultado); // Llama a la función parImpar y le pasa el resultado de la suma
+
     }
 
-    public static double suma(double num1, double num2) { // Define la función suma que toma dos parámetros num1 y num2
-        return num1 + num2; // Devuelve la suma de num1 y num2
+    public static double suma(double num1, double num2) {
+        return num1 + num2;
     }
 
-    public static void parImpar(double num) { // Define la función parImpar que toma un parámetro num
-        if (num % 2 == 0) { // Si el residuo de la división de num entre 2 es 0, entonces es par
-            System.out.println("El resultado es par.");
-        } else { // De lo contrario, es impar
-            System.out.println("El resultado es impar.");
+    public static void parImpar(double num) {
+        if (num % 2 == 0) {
+            System.out.println("Número par.");
+        } else {
+            System.out.println("Número impar.");
         }
     }
-}
 
+}
