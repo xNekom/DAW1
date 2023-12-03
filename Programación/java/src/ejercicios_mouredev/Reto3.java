504843687bce28d7e7ca61c2c132cfa7ad42bb37
xNekom <pedrojuuu@gmail.com>
2023-12-03 20:46:15 +0100

    .

--- Programación/java/src/ejercicios_mouredev/Reto3.java
+++ Programación/java/src/ejercicios_mouredev/Reto3.java
@@ -1,23 +1,23 @@
 package ejercicios_mouredev;
 
 public class Reto3 {
-    public static void main(String[] args) {
-        int numero, divisor;
-        int contadorDivisores;
 
-        for (numero = 1; numero <= 100; numero++) {
-            contadorDivisores = 1;
-            for (divisor = 2; divisor <= numero; divisor++) {
-                if (numero % divisor == 0) {
-                    contadorDivisores++;
-                }
-            }
-            if (contadorDivisores == 2){
-                System.out.println("Número primo; " + numero);
-            }
-            else {
-                System.out.println(numero);
-            }
+  public static void main(String[] args) {
+    int numero, divisor;
+    int contadorDivisores;
+
+    for (numero = 1; numero <= 100; numero++) {
+      contadorDivisores = 1;
+      for (divisor = 2; divisor <= numero; divisor++) {
+        if (numero % divisor == 0) {
+          contadorDivisores++;
         }
+      }
+      if (contadorDivisores == 2) {
+        System.out.println("Número primo; " + numero);
+      } else {
+        System.out.println(numero);
+      }
     }
+  }
 }
