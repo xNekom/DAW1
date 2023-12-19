28f1aadc3b196d209e6838f2dfc5d6450a3e35a9
xNekom <pedrojuuu@gmail.com>
2023-12-19 19:20:10 +0100

    .

--- Programación/java/src/java_array/aciertosLoteria.java
+++ Programación/java/src/java_array/aciertosLoteria.java
@@ -20,7 +20,6 @@ public class aciertosLoteria {
     public static void main(String[] args) {
         int[] apuesta = {45, 64, 31, 75, 1, 13};
         int[] combinacion = {43, 64, 65, 31, 11, 75};
-        Arrays.sort(apuesta);
         Arrays.sort(combinacion);
 
         int aciertos = aciertos(apuesta, combinacion);
